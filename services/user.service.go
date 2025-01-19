29bff45d3911a7c0a55b94893089620e55029b43
siddharth2440 <harshitsingh8427.live@gmail.com>
2025-01-19 22:35:35 +0530

    fixed the bug

--- services/user.service.go
+++ services/user.service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
@@ -27,6 +28,8 @@ func New_User_Service(db *mongo.Client) *User_Service_Struct {
 
 // Get MyProfile
 func (NUs *User_Service_Struct) Get_My_Profile(userId string) (*domain.User, error) {
+
+	fmt.Println("Inside the Get_My_profile Method...")
 	redis_client := utils.Get_Redis()
 	if redis_client == nil {
 		return nil, fmt.Errorf("redis connection failed")
@@ -57,43 +60,53 @@ func (NUs *User_Service_Struct) Get_My_Profile(userId string) (*domain.User, err
 			errChan <- err
 		}
 
-		fmt.Println("obj_Id")
-		fmt.Println(obj_Id)
+		// fmt.Println("obj_Id")
+		// fmt.Println(obj_Id)
 
 		to_get_User_details := bson.M{
 			"_id": obj_Id,
 		}
 
 		// chk in Redis
-		user_info_val := redis_client.Get("user_info" + user.ID.Hex()).Val()
-		// fmt.Println("user_info_val")
+		user_info_val := redis_client.Get("user_info" + userId).Val()
+		fmt.Println("user_info_val")
+		fmt.Println(user_info_val)
 
 		if user_info_val == "" {
+			fmt.Println("We didn;t get any value")
 			err = NUs.db.Database("ecommerce_golang").Collection("users").FindOne(ctx, to_get_User_details).Decode(&user)
 			if err != nil {
 				errChan <- err
 				return
 			}
 
-			// store this information to Redis
+			// Store this information to Redis
 			to_store_user_in_redis := &domain.User{
-				ID:       user.ID,
-				Username: user.Username,
-				Email:    user.Email,
-				Gender:   user.Gender,
+				ID:        user.ID,
+				Username:  user.Username,
+				Email:     user.Email,
+				Gender:    user.Gender,
+				IsAdmin:   user.IsAdmin,
+				CreatedAt: user.CreatedAt,
+				UpdatedAt: user.UpdatedAt,
 			}
 			err = redis_client.Set("user_info"+user.ID.Hex(), to_store_user_in_redis, 0).Err()
 			if err != nil {
 				errChan <- err
 			}
 			userInfo <- user
+		} else {
+			got_data_from_redis := redis_client.Get("user_info" + userId)
+			fmt.Println("got_data_from_redis")
+			fmt.Println(got_data_from_redis.Val())
+			// Decode Redis data into user struct
+			err = json.Unmarshal([]byte(got_data_from_redis.Val()), &user)
+			if err != nil {
+				errChan <- err
+				return
+			}
+			userInfo <- user
 		}
-
-		// else we're going to get the Data from our Redis Database
-		got_data_from_redis := redis_client.Get("user_info" + user.ID.Hex())
-		fmt.Println("got_data_from_redis")
-		fmt.Println(got_data_from_redis.Val())
-		userInfo <- user
 	}()
 
 	select {
