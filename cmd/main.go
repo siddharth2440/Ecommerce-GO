1c254e9041225c5ecfe34402002f957598f1dbe1
siddharth2440 <harshitsingh8427.live@gmail.com>
2025-02-13 06:40:43 +0530

    commit

--- cmd/main.go
+++ cmd/main.go
@@ -21,7 +21,7 @@ func main() {
 		log.Fatalf("Error in Setting up the DB connection: %v", err)
 	}
 	// router
-	fmt.Println("okay we good to go")
+	fmt.Println("okay w good to go")
 	router := routes.SetupRoutes(client)
 	router.Run(":8567")
 }
