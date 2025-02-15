49208e7e13071158c816da8c9bd904e8536c1b8a
siddharth2440 <harshitsingh8427.live@gmail.com>
2025-02-15 23:33:13 +0530

    checking pipeline

--- cmd/main.go
+++ cmd/main.go
@@ -21,7 +21,7 @@ func main() {
 		log.Fatalf("Error in Setting up the DB connection: %v", err)
 	}
 	// router
-	fmt.Println("okay werwe good to go")
+	fmt.Println("okay w erwe good to go")
 	router := routes.SetupRoutes(client)
 	router.Run(":8567")
 }
